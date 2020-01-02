9b2feabc36a35ba2e855abb4cf9b6d0c890b7c30
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-02 19:53:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
       @items << title
     end
     @total += price*quantity
-    @last_transaction_amount = @total
+    @last_transaction_amount = @total -= price
     @total
   end
   
