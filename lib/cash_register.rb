3da2fafdd708a5dbf753eb89e6ffca9f556a348b
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-02 19:52:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,20 @@ class CashRegister
     @last_transaction_amount = @total
     @total
   end
+  
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
 
   def apply_discount()
     if @discount > 0
